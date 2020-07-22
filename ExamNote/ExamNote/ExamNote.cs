d3e7750b70483769d06cf84306805e08d989cb67
영래 이 <hypoid99@gmail.com>
2020-07-22 19:20:58 +0900

    v0.1

---
+++ ExamNote/ExamNote/ExamNote.cs
@@ -0,0 +1,24 @@
+﻿// copyright honeysoft 2020 v0.1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamNote
+{
+    static class ExamNote
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainFrame());
+        }
+    }
+}
